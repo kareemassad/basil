8021db57628f42adc8cc94e16ffc5f76f3725c05
Chris Vettese <cvett018@uottawa.ca>
2020-02-09 05:25:54 -0500

    Updated health algorithm

--- src/recipeDetails.js
+++ src/recipeDetails.js
@@ -20,19 +20,8 @@ class RecipeDetails extends React.Component {
             this.props.history.push("/", {message: "You have been signed out."});
             return;
         }
-        let calories, vitamins = 0;
-        const recipe = this.props.location.state.recipe;
-        const keys = Object.keys(recipe.totalNutrients);
-        keys.map(key => {
-            console.log(key);
-            if (key === "ENERC_KCAL") {
-                calories = recipe.totalDaily[key].quantity * 4;
-            } else if (key === "VITA_RAE" || key === "VITC" || key === "VITD" || key === "VITK1") {
-                vitamins += recipe.totalDaily[key].quantity;
-                const newRating = (vitamins / calories) * 100.0;
-                this.setState({rating: Math.round(newRating) + "%"})
-            }
-        })
+        const health = this.calculateHealth(this.props.location.state.recipe);
+        this.setState({rating: health})
     }
 
     signOut() {
@@ -41,6 +30,26 @@ class RecipeDetails extends React.Component {
         }).catch(() => {
             this.props.history.push("/", {message: "Error signing out."});
         })
+    }
+
+    calculateHealth(recipe) {
+        let negative = 0, vitamins = 0, rating = 0, count = 0;
+        const keys = Object.keys(recipe.totalNutrients);
+        keys.map(key => {
+            console.log(key);
+            if (key === "ENERC_KCAL" || key === "NA") {
+                negative += recipe.totalDaily[key].quantity
+            } else if (key === "VITA_RAE" || key === "VITC" || key === "VITD" || key === "VITK1" || key === "FE" || key === "PROCNT") {
+                vitamins += recipe.totalDaily[key].quantity;
+                if (key === "PROCNT") vitamins += recipe.totalDaily[key].quantity;
+                count += 1;
+            }
+
+        });
+        negative *= count;
+        const newRating = (vitamins / negative) * 75;
+        rating = Math.round(newRating) + "%";
+        return rating;
     }
 
     showRecipeDetails() {
